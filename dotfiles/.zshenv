354bea8c2fce50fcb2a60d32b22d0d88f11ec324
Tatu Tolonen <tatu.tolonen@gmail.com>
2018-01-26 19:42:11 +0200

    kirjoitetaan profile

--- dotfiles/.zshenv
+++ dotfiles/.zshenv
@@ -1,4 +1,5 @@
 export PATH="/home/tatu/anaconda3/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 export PATH=$PATH:$HOME"/pycharm-current/bin"
 export PATH=$PATH:$HOME"/clion-current/bin"
 export AWT_TOOLKIT=MToolkit
--- install.sh
+++ install.sh
@@ -22,3 +22,16 @@ sudo make install
 chsh -s /bin/zsh
 
 git clone --depth 1 -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+if [[ ! -f $HOME/dotfiles/profile ]] ; then
+    echo "Kysytään asioita ja kirjoitetaan profiili"
+    echo "Polybar profiili: laptop, desktop?"
+    read polybar_value
+    echo "Wifi rajapinta?"
+    read wifi_interface
+    echo "ethernet rajapinta?"
+    read eth_interface
+    echo "TATU_SETUP_POLYBAR_BAR=$polybar_value" >> $HOME/dotfiles/profile
+    echo "TATU_SETUP_WIFI_INTERFAFCE=$wifi_interface" >> $HOME/dotfiles/profile
+    echo "TATU_SETUP_ETHERNET_INTERFACE=$eth_interface" >> $HOME/dotfiles/profile
+fi
